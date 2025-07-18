712afc7cfe755a072659157ba743c241e12c1ee0
valcir <vbalbino@emeal.nttdata.com>
2025-07-18 16:08:37 -0300

    remove region

--- envs/dev-gcp/main.tf
+++ envs/dev-gcp/main.tf
@@ -2,5 +2,4 @@ module "vpc" {
   source          = "git::https://github.com/nkopskimbodo/kimbodo-tf-multicloud-modules//gcp/vpc?ref=feature/add_gcp"
   vpc_name        = "posit-terraform-vpc"
   vpc_description = "VPC for Posit MVP infrastructure"
-  region          = var.region
 }
