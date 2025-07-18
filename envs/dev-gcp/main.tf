716588401b32c997d6d4b0ff9dc42774926d3c9a
valcir <vbalbino@emeal.nttdata.com>
2025-07-18 17:56:37 -0300

    add vpc

--- envs/dev-gcp/main.tf
+++ envs/dev-gcp/main.tf
@@ -3,3 +3,12 @@ module "vpc" {
   vpc_name        = "posit-terraform-vpc"
   vpc_description = "VPC for Posit MVP infrastructure"
 }
+module "subnet" {
+  source          = "git::https://github.com/nkopskimbodo/kimbodo-tf-multicloud-modules//gcp/subnet?ref=feature/add_gcp"
+  subnet_name     = "subnet-posit-us-central1"
+  primary_cidr    = "10.10.0.0/24"
+  pods_range_name = "gke-pods"
+  pods_cidr       = "10.136.0.0/14"
+  region          = var.region
+  vpc_id          = module.vpc.vpc_id
+}
