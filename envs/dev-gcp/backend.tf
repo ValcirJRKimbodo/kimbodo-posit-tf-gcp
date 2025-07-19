1c14507f28690a22bff96b19ddd7c96e53e0a37b
valcir <vbalbino@emeal.nttdata.com>
2025-07-19 08:35:52 -0300

    terraforrm fmt

--- envs/dev-gcp/backend.tf
+++ envs/dev-gcp/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "tfstate-posit-demo"
-    prefix = "dev"
+    bucket      = "tfstate-posit-demo"
+    prefix      = "dev"
     # credentials = "../../terraform-sa.json"
   }
 }
--- envs/dev-gcp/variables.tf
+++ envs/dev-gcp/variables.tf
@@ -4,45 +4,45 @@ variable "region" {
   default = "us-central1"
 }
 variable "vpc_name" {
-  type = string
-  default = "kimbodo-posit-vpc"  
+  type    = string
+  default = "kimbodo-posit-vpc"
 }
 variable "subnet_name" {
-  type = string
-  default = "kimbodo-posit-subnet"  
+  type    = string
+  default = "kimbodo-posit-subnet"
 }
 variable "reserved_ip_name" {
-  type = string
-  default = "kimbodo-posit-reserved-ip"  
+  type    = string
+  default = "kimbodo-posit-reserved-ip"
 }
-variable gke_name {
-  type = string
+variable "gke_name" {
+  type    = string
   default = "kimbodo-posit-cluster"
 }
 
 variable "primary_cdr" {
-  type = string
+  type    = string
   default = "10.10.0.0/24"
 }
 
 variable "pods_cdr" {
-  type = string
-  default = "10.136.0.0/14"  
+  type    = string
+  default = "10.136.0.0/14"
 }
-variable primary_nodepool_name {
-  type = string
+variable "primary_nodepool_name" {
+  type    = string
   default = "primary"
 }
-variable secondary_nodepool_name {
-  type = string
+variable "secondary_nodepool_name" {
+  type    = string
   default = "secondary"
 }
 
-variable primary_node_pool_macinhe {
-  type = string
+variable "primary_node_pool_macinhe" {
+  type    = string
   default = "e2-small"
 }
-variable secondary_node_pool_macinhe {
-  type = string
+variable "secondary_node_pool_macinhe" {
+  type    = string
   default = "n2-highmem-4"
 }
